03ffb970ba464129f7c88efd4aeef60ef8d6ed2b
Phạm Huy Hoàng <hoangphamhuy275132@gmail.com>
2024-03-18 21:23:58 +0700

    update project

--- src/main/java/com/javaweb/repository/impl/BuildingRepositoryImpl.java
+++ src/main/java/com/javaweb/repository/impl/BuildingRepositoryImpl.java
@@ -20,79 +20,112 @@ public class BuildingRepositoryImpl implements IBuildingRepository {
 	public List<BuildingEntity> findAll(Map<String, Object> params, List<String> typeCode) {
 		String sql = "SELECT * FROM building ";
 		String where = "WHERE 1 = 1";
+		String join = "";
+		//Name
 		if (params.containsKey("name") && params.get("name") != null && !params.get("name").toString().equals("")) {
 			String name = params.get("name").toString();
 			where += " AND name LIKE '%" + name + "%' ";
 		}
+		
+		//FloorArea
 		if (params.containsKey("floorarea") && params.get("floorarea") instanceof Integer) {
 			int floorArea = (Integer) params.get("floorarea");
 			where += " AND floorarea = " + floorArea + " ";
 		}
+		
+		//DistrictID
 		if (params.containsKey("districtid") && params.get("districtid") instanceof Integer) {
 			int districtID = (Integer) params.get("districtid");
-			sql += " INNER JOIN district on building.districtid = district.districtid ";
 			where += " AND districtid = " + districtID + " ";
 		}
+		if (params.containsKey("districtid") && params.get("districtid") instanceof Integer) {
+			join += " INNER JOIN district on building.districtid = district.id ";
+		}
+		
+		//Ward
 		if (params.containsKey("ward") && params.get("ward") != null && !params.get("ward").toString().equals("")) {
 			String ward = params.get("ward").toString();
 			where += " AND ward LIKE '%" + ward + "%' ";
 		}
-		if (params.containsKey("street") && params.get("street") != null
-				&& !params.get("street").toString().equals("")) {
+		
+		//Street
+		if (params.containsKey("street") && params.get("street") != null && !params.get("street").toString().equals("")) {
 			String street = params.get("street").toString();
 			where += " AND street LIKE '%" + street + "%' ";
 		}
+		
+		//NumberOfBasement
 		if (params.containsKey("numberofbasement") && params.get("numberofbasement") instanceof Integer) {
 			int numberOfBasement = (Integer) params.get("numberofbasement");
 			where += " AND numberofbasement = " + numberOfBasement + " ";
 		}
-		if (params.containsKey("direction") && params.get("direction") != null
-				&& !params.get("direction").toString().equals("")) {
+		
+		//Direction
+		if (params.containsKey("direction") && params.get("direction") != null && !params.get("direction").toString().equals("")) {
 			String direction = params.get("direction").toString();
 			where += " AND direction LIKE '%" + direction + "%' ";
 		}
-		if (params.containsKey("level") && params.get("level") != null
-				&& !params.get("level").toString().equals("")) {
+		
+		//Level
+		if (params.containsKey("level") && params.get("level") != null && !params.get("level").toString().equals("")) {
 			String level = params.get("level").toString();
 			where += " AND level LIKE '%" + level + "%' ";
 		}
+		
+		//MinArea
 		if (params.containsKey("minarea") && params.get("minarea") instanceof Integer) {
 			int minArea = (Integer) params.get("minarea");
-			sql += " INNER JOIN rentarea on building.buildingid = rentarea.buildingid ";
 			where += " AND value >= " + minArea + " ";
 		}
+		if (params.containsKey("minarea") && params.get("minarea") instanceof Integer) {
+			join += " INNER JOIN rentarea on building.id = rentarea.buildingid ";
+		}
+		
+		//MaxArea
 		if (params.containsKey("maxarea") && params.get("maxarea") instanceof Integer) {
 			int maxArea = (Integer) params.get("maxarea");
-			sql += " INNER JOIN rentarea on building.id = rentarea.buildingid ";
 			where += " AND value <= " + maxArea + " ";
 		}
+		if (params.containsKey("maxarea") && params.get("maxarea") instanceof Integer) {
+			join += " INNER JOIN rentarea on building.id = rentarea.buildingid ";
+		}
+		
+		//MinPrice
 		if (params.containsKey("minprice") && params.get("minprice") instanceof Integer ) {
 			Integer minPrice = ((Integer) params.get("minprice")).intValue();
 			where += " AND rentprice >= " + minPrice + " ";
 		}
+		
+		//MaxPrice
 		if (params.containsKey("maxprice") && params.get("maxprice") instanceof Integer) {
 			int maxPrice = (int) params.get("maxprice");
 			where += " AND rentprice <= " + maxPrice + " ";
 		}
-		if (params.containsKey("managername") && params.get("managername") != null
-				&& !params.get("managername").toString().equals("")) {
+		
+		//ManagerName
+		if (params.containsKey("managername") && params.get("managername") != null && !params.get("managername").toString().equals("")) {
 			String managerName = params.get("managername").toString();
 			where += " AND managername LIKE '%" + managerName + "%' ";
 		}
-		if (params.containsKey("managerphonenumber") && params.get("managerphonenumber") != null
-				&& !params.get("managerphonenumber").toString().equals("")) {
+		
+		//ManagerPhoneNumber
+		if (params.containsKey("managerphonenumber") && params.get("managerphonenumber") != null && !params.get("managerphonenumber").toString().equals("")) {
 			String managerPhonenumber = params.get("managerphonenumber").toString();
 			where += " AND managerphonenumber LIKE '%" + managerPhonenumber + "%' ";
 		}
+		
+		//StaffID
 		if (params.containsKey("staffid") && params.get("staffid") instanceof Integer) {
 			int staffID = ((Integer) params.get("staffid")).intValue();
-			sql += " INNER JOIN assignmentbuilding on building.id = assignmentbuilding.buildingid "
-					+ " INNER JOIN user on assignmentbuilding.staffid = user.id";
 			where += " AND staffid = " + staffID + " ";
 		}
+		if (params.containsKey("staffid") && params.get("staffid") instanceof Integer) {
+			join += " INNER JOIN assignmentbuilding on building.id = assignmentbuilding.buildingid "
+					+ " INNER JOIN user on assignmentbuilding.staffid = user.id ";
+		}
+		
+		//TypeCode
 		if (typeCode != null && !typeCode.isEmpty()) {
-			sql += " INNER JOIN buildingrenttype on building.id = buildingrenttype.buildingid "
-					+ " INNER JOIN renttype on buildingrenttype.renttypeid = renttype.id";
 			StringBuilder typeCondition = new StringBuilder(" AND code IN (");
 			for (String code : typeCode) {
 				typeCondition.append("'").append(code).append("',");
@@ -101,7 +134,12 @@ public class BuildingRepositoryImpl implements IBuildingRepository {
 			typeCondition.append(")");
 			where += typeCondition.toString();
 		}
-		sql += where;
+		if (typeCode != null && !typeCode.isEmpty()) {
+			join += " INNER JOIN buildingrenttype on building.id = buildingrenttype.buildingid "
+					+ " INNER JOIN renttype on buildingrenttype.renttypeid = renttype.id";
+		}
+		
+		sql = sql + join + where;
 		List<BuildingEntity> result = new ArrayList<BuildingEntity>();
 		try (Connection conn = ConnectionUtils.getConnection();
 				Statement stm = conn.createStatement();
